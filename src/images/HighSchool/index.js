4fe9802f8cd8d9597f8a5dfad46166e6b0532a81
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-06-08 18:42:31 +0200

    high school page images

---
+++ src/images/HighSchool/index.js
@@ -0,0 +1,7 @@
+import Analytics from './Analytics.svg'
+import Assessment from './Assessment.svg'
+import Clock from './Clock.svg'
+import Settings from './Settings.svg'
+import FaceToFace from './FaceToFace.svg'
+
+export { Analytics, Assessment, Clock, Settings, FaceToFace }
