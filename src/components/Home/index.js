a51bd47d7362b4895ebc362eba844dabc44d953c
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-06-08 11:12:52 +0200

    updated sections on home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,5 +2,16 @@ import BannerFirst from './BannerFirst'
 import BannerSecond from './BannerSecond'
 import Applications from './Applications'
 import Features from './Features'
+import Clients from './Clients'
+import Timeline from './Timeline'
+import BlogNews from './BlogNews'
 
-export { BannerFirst, BannerSecond, Applications, Features }
+export {
+  BannerFirst,
+  BannerSecond,
+  Applications,
+  Features,
+  Clients,
+  Timeline,
+  BlogNews,
+}
