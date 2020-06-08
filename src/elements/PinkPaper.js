de156225846ce84ee3da894667b9cf6aca333b3c
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-06-08 11:18:29 +0200

    updated PinkPaper element

--- src/elements/PinkPaper.js
+++ src/elements/PinkPaper.js
@@ -2,19 +2,19 @@ import Paper from '@material-ui/core/Paper'
 import styled from 'styled-components'
 import theme from '../gatsby-theme-material-ui-top-layout/theme'
 
-// const PinkPaper = styled(Paper)`
-//   background-image: ${theme.palette.primary.mainGradient};
-//   color: ${theme.palette.primary.color};
-//   border-radius: 0;
-//   padding: 2%;
-//   margin: 5% 0;
-// `
+const PinkPaper = styled(Paper)`
+  background-image: ${theme.palette.primary.mainGradient};
+  color: ${theme.palette.primary.color};
+  border-radius: 0;
+  padding: 2%;
+  margin: 5% 0;
+`
 
-const PinkPaper = styled(Paper)({
-  color: 'primary',
-  borderRadius: 0,
-  padding: '2%',
-  margin: '5% 0',
-})
+// const PinkPaper = styled(Paper)({
+//   color: 'primary',
+//   borderRadius: 0,
+//   padding: '2%',
+//   margin: '5% 0',
+// })
 
 export default PinkPaper
