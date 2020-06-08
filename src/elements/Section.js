2d267c8ebe05eb4655c98869ba442a0039473774
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-06-08 11:24:03 +0200

    improvement on section element, to continue

--- src/elements/Section.js
+++ src/elements/Section.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Container, useTheme } from '@material-ui/core'
+import { useTheme } from '@material-ui/core'
 import theme from '../gatsby-theme-material-ui-top-layout/theme'
 
 const SectionStyle = styled.section`
   height: 100vh;
   background-color: ${props => props.backgroundColor};
-  ${theme.breakpoints.down('md')} {
+  ${theme.breakpoints.down('sm')} {
     height: auto;
   }
 `
