e0eb7778477872f37d50ce3ee6486b595213bb04
Maria Eugenia Trapani <meugenia.trapani@gmail.com>
2020-06-12 14:42:23 +0200

    clients pages

---
+++ src/images/University/index.js
@@ -0,0 +1,6 @@
+import Economic from './Economic.svg'
+import LmsFriendly from './LmsFriendly.svg'
+import Population from './Population.svg'
+import Support from './Support.svg'
+
+export { Economic, LmsFriendly, Population, Support }
---
+++ src/pages/university.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Layout, SEO } from '../components'
+
+import { useStaticQuery, graphql } from 'gatsby'
+
+import { Grid } from '@material-ui/core'
+import {
+  ImageLeft,
+  BenefitsImages,
+  ExpansivePannel,
+} from '../components/Clients'
+import {
+  Economic,
+  LmsFriendly,
+  Population,
+  Support,
+} from '../images/University'
+
+const University = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      University: file(relativePath: { eq: "University/University.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 800) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="hola" />
+      <Grid container spacing={3} alignItems="center">
+        <ImageLeft
+          variantTitle="h1"
+          title="Higher education  and entry exam"
+          description="Whether it is on continuous checks or a one-shot exams with large populations, TestWe adapts and offers you a simple creation and management tool for your exams. The solution will help you to have a qualitative reporting of your students and candidates."
+          alt="alt"
+          fluid={data.University.childImageSharp.fluid}
+        />
+        <BenefitsImages
+          firstSvg={LmsFriendly}
+          firstTitle="LMS friendly"
+          secondSvg={Support}
+          secondTitle="Technical support"
+          thirdSvg={Population}
+          thirdTitle="Large population"
+          fourthSvg={Economic}
+          fourthTitle="Economic"
+          fifthSvg={Economic}
+          fifthTitle="Analytics"
+        />
+        <ExpansivePannel />
+      </Grid>
+    </Layout>
+  )
+}
+
+export default University
